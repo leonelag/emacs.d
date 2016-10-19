6cb4d3fe74d4e98d78489fcaf4b31955766d08ac
Leonel Gayard <leonel.gayard@gmail.com>
2016-10-19 01:15:02 +0100

    Default font, remove tool bars, some global keys.

--- init.el
+++ init.el
@@ -6,12 +6,29 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
-(set-default-font "-outline-Consolas-normal-normal-normal-mono-18-*-*-*-c-*-iso8859-1")
+; (set-default-font "-outline-Consolas-normal-normal-normal-mono-18-*-*-*-c-*-iso8859-1")
+(set-default-font "Consolas 11")
 
 (package-initialize)
 
-(global-set-key [C-\;] 'delete-horizontal-space)
-(global-set-key [C-.] 'join-line)
+(global-set-key (kbd "C-;") 'delete-horizontal-space)
+(global-set-key (kbd "C-.") 'join-line)
+
+;;
+;; https://masteringemacs.org/article/introduction-to-ido-mode
+;;
+(setq ido-use-filename-at-point 'guess)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;;
+;; Magit. http://magit.vc/manual/magit/Getting-started.html#Getting-started
+;;
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 
 
 
